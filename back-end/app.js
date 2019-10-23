55995646fb18e327ea8d694e64c9a7d947f094e4
unknown <jupiter85838@yahoo.com>
2019-10-23 18:28:45 -0400

    adding custom errors using formatError

--- back-end/app.js
+++ back-end/app.js
@@ -47,7 +47,21 @@ app.use((req, res, next) => {
 app.use("/graphql", graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql:true
+    graphiql: true,
+    formatError(err) {
+        if (!err.originalError) {
+            return err
+        }
+        const data = err.originalError.data;
+        const message = err.message || "An Error Occured!";
+        const code = err.originalError.code | 500;
+        return ({
+            message: message,
+            status: code,
+            data: data
+        })
+
+    }
 
 }))
 
--- back-end/graphql/resolvers.js
+++ back-end/graphql/resolvers.js
@@ -19,6 +19,8 @@ module.exports = {
         }
         if (errors.length > 0) {
             const error = new Error("Invalide input!");
+            error.data=errors;
+            error.code=422;  
             throw error
         }
         const hashedPw = await bcrypt.hash(userInput.password, 12);
