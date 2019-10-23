810e0bd3fecb16347ad673b0dbe4cfd7def7f3c0
unknown <jupiter85838@yahoo.com>
2019-10-23 18:14:38 -0400

    add validation to resolver

--- back-end/graphql/resolvers.js
+++ back-end/graphql/resolvers.js
@@ -1,11 +1,25 @@
 const User = require("../models/user");
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 module.exports = {
     createUser: async function ({ userInput }, req) {
         const existingUser = await User.findOne({ email: userInput.email })
+        const errors = [];
+        if (!validator.isEmail(userInput.email)) {
+
+            errors.push({ message: "E-Mail is invalid!" })
+
+        }
+        if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+            errors.push({ message: "password too short" })
+        }
         if (existingUser) {
             const error = new Error("User Exists Already!");
             throw error;
+        }
+        if (errors.length > 0) {
+            const error = new Error("Invalide input!");
+            throw error
         }
         const hashedPw = await bcrypt.hash(userInput.password, 12);
         const user = new User({
