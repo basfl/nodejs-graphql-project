447457deb92dd6b00facf73aebb0bb7d2455a8b1
unknown <jupiter85838@yahoo.com>
2019-10-26 17:10:50 -0400

    add new mutation and extract data from token

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 http://localhost:8080/graphql
+
+# Note
+ * difference between query and mutation :
+    * In simple words the query is SELECT statement and mutation is INSERT Operation.
+    Query in graphql is used to fetch data while mutation is used for INSERT/UPDATE/DELETE operation.
--- back-end/graphql/schema.js
+++ back-end/graphql/schema.js
@@ -21,6 +21,11 @@ module.exports = buildSchema
             posts:[Post!]
 
         }
+        type PostInputData {
+            title:String!
+            content:String!
+            imageUrl:String!
+        }
         type AuthData {
             token:String!
             userId:String!
@@ -35,6 +40,7 @@ module.exports = buildSchema
         }
       type RootMutation {
         createUser(userInput:UserInputData): User!
+        createPost(postInput:PostInputData): Post!
       }
         schema {
             query:RootQuery
